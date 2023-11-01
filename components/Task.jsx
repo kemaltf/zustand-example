ea8148a921ab48b738ce4fb3069438be01c16fff
E127214 <kemal.taufik@aia.com>
2023-11-01 13:55:55 +0800

    Creating the basic component

---
+++ components/Task.jsx
@@ -0,0 +1,15 @@
+import classNames from 'classnames';
+import './Task.css';
+
+const STATUS = '';
+export default function Task({ title }) {
+  return (
+    <div className="task">
+      <div>{title}</div>
+      <div className="bottomWrapper">
+        <div></div>
+        <div className={classNames('status', STATUS)}>{STATUS}</div>
+      </div>
+    </div>
+  );
+}
