16d0073b45516b1f87448110cc74b3d0387f02c5
E127214 <kemal.taufik@aia.com>
2023-11-01 13:43:46 +0800

    create the basic component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import Column from '../../components/Column';
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <Column state="PLANNED"></Column>
+      <Column state="ONGOING"></Column>
+      <Column state="DONE"></Column>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
