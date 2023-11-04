73b7055541bea24bb71dff7ad2f0395854ae899f
kemaltf <kemaltaufikf@gmail.com>
2023-11-04 20:09:23 +0700

    Trying displaying another task

--- src/store.js
+++ src/store.js
@@ -5,7 +5,11 @@ import { create } from 'zustand';
 // and it will return an object (contain everything we have in our store)
 // inside this, basically globalized our tasks
 const store = (set) => ({
-  tasks: [{ title: 'TestTask', state: 'PLANNED' }],
+  tasks: [
+    { title: 'Test 1 Task', state: 'PLANNED' },
+    { title: 'Test 2 Task', state: 'ONGOING' },
+    { title: 'Test 3 Task', state: 'DONE' },
+  ],
 });
 
 // ask to chat gpt when we export this it will be like what?
